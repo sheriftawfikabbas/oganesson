801acb0426aa21835dd9d2822645d13a1fb2be9c
Sherif Abdulkader Tawfik Abbas <53269434+sheriftawfikabbas@users.noreply.github.com>
2023-05-17 10:37:11 +1000

    edits

--- oganesson/__init__.py
+++ oganesson/__init__.py
@@ -1,4 +1,13 @@
 from oganesson.ogai import OgAI
-VERSION = '0.1.1'
+from oganesson.descriptors.bacd import BACD 
+from oganesson.descriptors.rosa import ROSA
+from oganesson.descriptors.symmetry_functions import SymmetryFunctions
+from oganesson.ogstructure import OgStructure
+from oganesson.genetic_algorithms import GA
 
-__all__ = ("OgAI")
+__all__ = ("OgAI",
+           "BACD",
+           "ROSA",
+           "SymmetryFunctions",
+           "OgStructure",
+           "GA")
