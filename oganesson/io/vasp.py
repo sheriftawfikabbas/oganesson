47143c33e7b0f30434f89337f80a69aafd1465e8
Sherif Abbas <53269434+sheriftawfikabbas@users.noreply.github.com>
2023-06-13 18:19:18 +1000

    Update vasp.py

--- oganesson/io/vasp.py
+++ oganesson/io/vasp.py
@@ -2,69 +2,86 @@ from oganesson.ogstructure import OgStructure
 from pymatgen.core import Structure
 import numpy as np
 
-def outcar_extractor(outcar_directory:str=".",outcar_file:str='OUTCAR'):
-    def get_indices(l, s):
-        indices = []
-        for i in range(len(l)):
-            if s in l[i]:
-                indices += [i]
-        return indices
+class Outcar:
+    def __init__(self, outcar_directory:str=".", outcar_file:str='OUTCAR') -> None:
+        self.outcar_directory = outcar_directory
+        self.outcar_file = outcar_file
 
-    def get_vectors(l, positions, displacement, rows, columns):
-        vectors = []
-        for position in positions:
-            vector = []
-            s = l[position+displacement:position+displacement+rows]
-            for r in s:
-                c = r.split()
-                c = c[0:columns]
-                c = [float(x) for x in c]
-                vector += [c]
-            vectors += [vector]
-        return vectors
+    def get_md_data(self):
+        return self._outcar_extractor()
+    
+    def write_md_data(self, file_name:str = None, path:str='.'):
+        structures, forces_vectors, stress_matrices = self._outcar_extractor()
+        if file_name is None:
+            file_name = self.outcar_file+'.json'
+        output = {'structures':structures,'forces':forces_vectors, 'stresses':stress_matrices}
+        import json
+        with open(path+file_name,'w') as f:
+            json.dump(output, f)
 
-    def get_atoms_counts(l):
-        ion_counts = []
-        ions = []
-        for s in l:
-            if 'ions per type' in s:
-                s = s.replace('ions per type', '').replace('=', '').split()
-                ion_counts = [int(x) for x in s]
-                break
-        for s in l:
-            if 'POSCAR =' in s:
-                s = s.replace('POSCAR =', '').split()
-                ions = s
-        return ion_counts, ions
+    def _outcar_extractor(self):
+        def get_indices(l, s):
+            indices = []
+            for i in range(len(l)):
+                if s in l[i]:
+                    indices += [i]
+            return indices
 
-    outcarf = open(outcar_directory+outcar_file, 'r')
-    outcar = outcarf.readlines()
-    outcarf.close()
+        def get_vectors(l, positions, displacement, rows, columns):
+            vectors = []
+            for position in positions:
+                vector = []
+                s = l[position+displacement:position+displacement+rows]
+                for r in s:
+                    c = r.split()
+                    c = c[0:columns]
+                    c = [float(x) for x in c]
+                    vector += [c]
+                vectors += [vector]
+            return vectors
 
-    structures = []
-    ion_counts, ions = get_atoms_counts(outcar)
-    atoms = []
-    for i in range(len(ions)):
-        atoms += ion_counts[i]*[ions[i]]
+        def get_atoms_counts(l):
+            ion_counts = []
+            ions = []
+            for s in l:
+                if 'ions per type' in s:
+                    s = s.replace('ions per type', '').replace('=', '').split()
+                    ion_counts = [int(x) for x in s]
+                    break
+            for s in l:
+                if 'POSCAR =' in s:
+                    s = s.replace('POSCAR =', '').split()
+                    ions = s
+            return ion_counts, ions
 
-    number_of_atoms = sum(ion_counts)
+        outcarf = open(self.outcar_directory+self.outcar_file, 'r')
+        outcar = outcarf.readlines()
+        outcarf.close()
 
-    direct_lattice_vectors_positions = get_indices(
-        outcar, 'direct lattice vectors')
-    positions_forces_vectors_positions = get_indices(
-        outcar, 'TOTAL-FORCE (eV/Angst)')
-    stresses_positions = get_indices(
-        outcar, 'stress matrix')
-    lattice_vectors = get_vectors(
-        outcar, direct_lattice_vectors_positions, 1, 3, 3)
-    positions_forces_vectors = np.array(get_vectors(
-        outcar, positions_forces_vectors_positions, 2, number_of_atoms, 6))
-    positions_vectors = positions_forces_vectors[:, :, 0:3]
-    forces_vectors = positions_forces_vectors[:, :, 3:6]
-    stress_matrices = get_vectors(
-        outcar, stresses_positions, 1, 3, 3)
-    for i in range(len(positions_vectors)):
-        structures += [OgStructure(Structure(species=atoms,
-                                             coords=positions_vectors[i], lattice=lattice_vectors[i]))]
+        structures = []
+        ion_counts, ions = get_atoms_counts(outcar)
+        atoms = []
+        for i in range(len(ions)):
+            atoms += ion_counts[i]*[ions[i]]
 
-    return structures, forces_vectors, stress_matrices
+        number_of_atoms = sum(ion_counts)
+
+        direct_lattice_vectors_positions = get_indices(
+            outcar, 'direct lattice vectors')
+        positions_forces_vectors_positions = get_indices(
+            outcar, 'TOTAL-FORCE (eV/Angst)')
+        stresses_positions = get_indices(
+            outcar, 'stress matrix')
+        lattice_vectors = get_vectors(
+            outcar, direct_lattice_vectors_positions, 1, 3, 3)
+        positions_forces_vectors = np.array(get_vectors(
+            outcar, positions_forces_vectors_positions, 2, number_of_atoms, 6))
+        positions_vectors = positions_forces_vectors[:, :, 0:3]
+        forces_vectors = positions_forces_vectors[:, :, 3:6]
+        stress_matrices = get_vectors(
+            outcar, stresses_positions, 1, 3, 3)
+        for i in range(len(positions_vectors)):
+            structures += [OgStructure(Structure(species=atoms,
+                                                coords=positions_vectors[i], lattice=lattice_vectors[i]))]
+
+        return structures, forces_vectors, stress_matrices
