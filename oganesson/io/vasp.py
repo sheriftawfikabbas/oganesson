acb5d069b55beb6204ab044a30650b4942765893
Sherif Abbas <53269434+sheriftawfikabbas@users.noreply.github.com>
2024-06-17 11:34:21 +1000

    added Potcar handling

--- oganesson/io/vasp.py
+++ oganesson/io/vasp.py
@@ -3,11 +3,12 @@ from oganesson.ogstructure import OgStructure
 from pymatgen.io.vasp import Poscar as MPPoscar
 import numpy as np
 
+
 class Poscar:
     def __init__(self, structure) -> None:
         self.ogstructure = OgStructure(structure)
-    
-    def freeze_up_to(self,z):
+
+    def freeze_up_to(self, z):
         """
         Produce the POSCAR file after modifying it to constraint the layers of atoms below z Angstroms
         """
@@ -15,31 +16,68 @@ class Poscar:
         for i in range(len(self.ogstructure)):
             atom = self.ogstructure().cart_coords[i]
             if atom[2] < z:
-                constraints[i,:] = False
+                constraints[i, :] = False
             else:
-                constraints[i,:] = True
-        mpposcar = MPPoscar(self.ogstructure(),selective_dynamics=constraints)
+                constraints[i, :] = True
+        mpposcar = MPPoscar(self.ogstructure(), selective_dynamics=constraints)
         return mpposcar.get_string()
-        
+
+
+class Potcar:
+    def __init__(self, file_name="POTCAR") -> None:
+        f = open(file_name)
+        potcar_content = f.read()
+        f.close
+        self.element_data = {}
+        blocks = potcar_content.strip().split("TITEL")
+        blocks = blocks[1:]
+        for block in blocks:
+            block_lines = block.splitlines()
+            a = block_lines[0].split()[2]
+            a = a.split("_")[0]
+            if a in self.element_data.keys():
+                # Skip the next block
+                continue
+            else:
+                self.element_data[a] = {}
+            for line in block_lines:
+                line = line.replace(";", "")
+                if "ZVAL" in line:
+                    self.element_data[a]["ZVAL"] = float(line.split()[5])
+                if "RCORE" in line:
+                    self.element_data[a]["RCORE"] = float(line.split()[2])
+                if "POMASS" in line:
+                    self.element_data[a]["POMASS"] = float(line.split()[2])
+
 
 class Outcar:
-    def __init__(self, outcar_directory:str="./", outcar_file:str='OUTCAR', poscar_file:str=None) -> None:
+    def __init__(
+        self,
+        outcar_directory: str = "./",
+        outcar_file: str = "OUTCAR",
+        poscar_file: str = None,
+    ) -> None:
         self.outcar_directory = outcar_directory
         self.outcar_file = outcar_file
         self.poscar_file = poscar_file
 
     def get_md_data(self):
         return self._outcar_extractor()
-    
-    def write_md_data(self, file_name:str = None, path:str=None):
+
+    def write_md_data(self, file_name: str = None, path: str = None):
         structures, forces_vectors, stress_matrices = self._outcar_extractor()
         if file_name is None:
-            file_name = self.outcar_file+'.json'
+            file_name = self.outcar_file + ".json"
         if path is None:
             path = self.outcar_directory
-        output = {'structures':structures,'forces':forces_vectors, 'stresses':stress_matrices}
+        output = {
+            "structures": structures,
+            "forces": forces_vectors,
+            "stresses": stress_matrices,
+        }
         import json
-        with open(path+file_name,'w') as f:
+
+        with open(path + file_name, "w") as f:
             json.dump(output, f)
 
     def _outcar_extractor(self):
@@ -54,7 +92,7 @@ class Outcar:
             vectors = []
             for position in positions:
                 vector = []
-                s = l[position+displacement:position+displacement+rows]
+                s = l[position + displacement : position + displacement + rows]
                 for r in s:
                     c = r.split()
                     c = c[0:columns]
@@ -67,63 +105,71 @@ class Outcar:
             ion_counts = []
             ions = []
             for s in l:
-                if 'ions per type' in s:
-                    s = s.replace('ions per type', '').replace('=', '').split()
+                if "ions per type" in s:
+                    s = s.replace("ions per type", "").replace("=", "").split()
                     ion_counts = [int(x) for x in s]
                     break
             for s in l:
-                if 'POSCAR =' in s:
-                    s = s.replace('POSCAR =', '').split()
+                if "POSCAR =" in s:
+                    s = s.replace("POSCAR =", "").split()
                     ions = s
             return ion_counts, ions
 
-        outcarf = open(self.outcar_directory+self.outcar_file, 'r')
+        outcarf = open(self.outcar_directory + self.outcar_file, "r")
         outcar = outcarf.readlines()
         outcarf.close()
 
         structures = []
         atoms = []
         if self.poscar_file is not None:
-            structure = Structure.from_file(self.outcar_directory+self.poscar_file)
+            structure = Structure.from_file(self.outcar_directory + self.poscar_file)
             number_of_atoms = len(structure)
             atoms = [s.specie.symbol for s in structure]
         else:
             ion_counts, ions = get_atoms_counts(outcar)
-        
+
             for i in range(len(ions)):
-                atoms += ion_counts[i]*[ions[i]]
+                atoms += ion_counts[i] * [ions[i]]
 
             number_of_atoms = sum(ion_counts)
 
-        direct_lattice_vectors_positions = get_indices(
-            outcar, 'direct lattice vectors')
+        direct_lattice_vectors_positions = get_indices(outcar, "direct lattice vectors")
         positions_forces_vectors_positions = get_indices(
-            outcar, 'TOTAL-FORCE (eV/Angst)')
-        stresses_positions = get_indices(
-            outcar, 'stress matrix')
-        lattice_vectors = get_vectors(
-            outcar, direct_lattice_vectors_positions, 1, 3, 3)
-        positions_forces_vectors = np.array(get_vectors(
-            outcar, positions_forces_vectors_positions, 2, number_of_atoms, 6))
+            outcar, "TOTAL-FORCE (eV/Angst)"
+        )
+        stresses_positions = get_indices(outcar, "stress matrix")
+        lattice_vectors = get_vectors(outcar, direct_lattice_vectors_positions, 1, 3, 3)
+        positions_forces_vectors = np.array(
+            get_vectors(
+                outcar, positions_forces_vectors_positions, 2, number_of_atoms, 6
+            )
+        )
         positions_vectors = positions_forces_vectors[:, :, 0:3]
         forces_vectors = positions_forces_vectors[:, :, 3:6]
-        stress_matrices = get_vectors(
-            outcar, stresses_positions, 1, 3, 3)
+        stress_matrices = get_vectors(outcar, stresses_positions, 1, 3, 3)
         for i in range(len(positions_vectors)):
-            structures += [Structure(species=atoms,
-                                                coords=positions_vectors[i], lattice=lattice_vectors[i], coords_are_cartesian=True).as_dict()]
+            structures += [
+                Structure(
+                    species=atoms,
+                    coords=positions_vectors[i],
+                    lattice=lattice_vectors[i],
+                    coords_are_cartesian=True,
+                ).as_dict()
+            ]
 
         return structures, forces_vectors.tolist(), stress_matrices
 
     def get_maximum_force(self):
-        TAG = 'TOTAL-FORCE (eV/Angst)'
+        TAG = "TOTAL-FORCE (eV/Angst)"
         if self.poscar_file is None:
-            print('og:Must supply a POSCAR file along with the OUTCAR file to extract the maximum force.')
+            print(
+                "og:Must supply a POSCAR file along with the OUTCAR file to extract the maximum force."
+            )
             return None
-        outcarf = open(self.outcar_file, 'r')
+        outcarf = open(self.outcar_file, "r")
         outcar = outcarf.readlines()
         outcarf.close()
-        poscarf = open(self.poscar_file, 'r')
+        poscarf = open(self.poscar_file, "r")
         poscar = poscarf.readlines()
         poscarf.close()
         num_atoms = sum([int(x) for x in poscar[6].split()])
@@ -132,20 +178,34 @@ class Outcar:
             if TAG in outcar[il]:
                 list_of_tags += [il]
         i = list_of_tags[-1]
-        if i >= len(outcar) or i+2+num_atoms >= len(outcar):
+        if i >= len(outcar) or i + 2 + num_atoms >= len(outcar):
             i = list_of_tags[-2]
-        lines = outcar[i+2:i+2+num_atoms]
+        lines = outcar[i + 2 : i + 2 + num_atoms]
         forces = np.array([l.split()[3:6] for l in lines]).astype(np.float64)
-        print('Maximum force =', forces.max())
+        print("Maximum force =", forces.max())
         return forces.max()
 
 
-def get_bandgap(vasp_folder='./'):
+def get_nelect(vasp_folder="./"):
     try:
-        feigenval = open(vasp_folder+'EIGENVAL', 'r')
+        og = OgStructure(file_name=vasp_folder + "/POSCAR")
+        potcar = Potcar(file_name=vasp_folder + "/POTCAR")
+        nelect = 0
+        for m in potcar.element_data.keys():
+            nelect += (
+                og.structure.composition.as_dict()[m] * potcar.element_data[m]["ZVAL"]
+            )
+        return nelect
+    except Exception as e:
+        print("Problem processing VASP input files",e)
+
+
+def get_bandgap(vasp_folder="./"):
+    try:
+        feigenval = open(vasp_folder + "EIGENVAL", "r")
         eigenval = feigenval.readlines()
         feigenval.close()
-        fdoscar = open(vasp_folder+'DOSCAR', 'r')
+        fdoscar = open(vasp_folder + "DOSCAR", "r")
         doscar = fdoscar.readlines()
         fdoscar.close()
         fermi = float(doscar[5].split()[3])
@@ -157,9 +217,9 @@ def get_bandgap(vasp_folder='./'):
         CB = []
 
         for b in range(num_bands):
-            vals = eigenval[7 + (num_points+2)* b:7 + (num_points+2)*(b+1)]
-            vals = vals [1:-1]
-            vals = np.array([v.split() for v in vals])[:,1].astype(np.float64)
+            vals = eigenval[7 + (num_points + 2) * b : 7 + (num_points + 2) * (b + 1)]
+            vals = vals[1:-1]
+            vals = np.array([v.split() for v in vals])[:, 1].astype(np.float64)
             VB += [vals[vals <= fermi]]
             CB += [vals[vals > fermi]]
 
